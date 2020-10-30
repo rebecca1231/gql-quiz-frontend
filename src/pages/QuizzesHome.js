f75e96ccf3d5ebe71fd1e779217830eb38591a4e
Rebecca <rebeccagetsanemail@gmail.com>
2020-10-30 16:01:04 +0900

    edited landing

--- src/pages/QuizzesHome.js
+++ src/pages/QuizzesHome.js
@@ -8,15 +8,23 @@ import { FETCH_QUIZZES_QUERY } from "../util/graphql";
 // import { AuthContext } from "../context/auth";
 
 const QuizzesHome = () => {
- 
-
   // const { user } = useContext(AuthContext);
   const { loading, data: { getQuizzes: quizzes } = {} } = useQuery(
     FETCH_QUIZZES_QUERY
   );
 
   return (
-    <Grid columns='2'>
+    <Grid columns="2">
+      <Grid.Row className="page-title">
+        <h1>Welcome to Movie Quiz!</h1>
+        <p style={{ fontSize: "1.35rem" }}>
+          {" "}
+          You can create a quiz, or play any of the ones below!
+        </p>
+        <p style={{ fontSize: "1.35rem" }}>
+          If you want to save, comment or like a quiz, you'll need to login.
+        </p>
+      </Grid.Row>
       <Grid.Row className="page-title">
         <h2>Recent Quizzes</h2>
       </Grid.Row>
@@ -27,8 +35,8 @@ const QuizzesHome = () => {
           <Transition.Group>
             {quizzes &&
               quizzes.map((quiz) => (
-                <Grid.Column key={quiz.id}  style={{ marginBottom: "20px" }}>
-                  <QuizCard  quiz={quiz} />
+                <Grid.Column key={quiz.id} style={{ marginBottom: "20px" }}>
+                  <QuizCard quiz={quiz} />
                 </Grid.Column>
               ))}
           </Transition.Group>
