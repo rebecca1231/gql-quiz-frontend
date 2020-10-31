2dca2f8c7523767666ec1040264a245a8ac68351
Rebecca <rebeccagetsanemail@gmail.com>
2020-10-31 16:42:56 +0900

    added welcome video

--- src/pages/QuizzesHome.js
+++ src/pages/QuizzesHome.js
@@ -4,11 +4,11 @@ import { Grid, Transition, Loader } from "semantic-ui-react";
 
 import QuizCard from "../components/QuizCard";
 import { FETCH_QUIZZES_QUERY } from "../util/graphql";
+import MovieQuizMovie from '../MovieQuizMovie.mov'
+import learnmore from '../learnmore.png'
 
-// import { AuthContext } from "../context/auth";
 
 const QuizzesHome = () => {
-  // const { user } = useContext(AuthContext);
   const { loading, data: { getQuizzes: quizzes } = {} } = useQuery(
     FETCH_QUIZZES_QUERY
   );
@@ -24,7 +24,13 @@ const QuizzesHome = () => {
         <p style={{ fontSize: "1.35rem" }}>
           If you want to save, comment or like a quiz, you'll need to login.
         </p>
-      </Grid.Row>
+        <video
+        src={MovieQuizMovie}
+        autoPlay="false"
+        poster={learnmore}
+        style={{ display: "block", margin: "1rem auto 1rem auto", width: "80vw", maxWidth:"400px" }}
+        controls="controls"
+      />      </Grid.Row>
       <Grid.Row className="page-title">
         <h2>Recent Quizzes</h2>
       </Grid.Row>
